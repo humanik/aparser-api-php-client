1a1c5e5494354a689ae12f29cff812e638475e75
humanik <humanikdev@gmail.com>
2022-02-04 09:42:26 +0300

    Change

--- lib/Parser.php
+++ lib/Parser.php
@@ -47,7 +47,7 @@ class Parser
          ]);
     }
 
-    public function addFilter(string $field, string $pattern, string $type = 'equal', string $option = 'sens'): void
+    public function addFilter(string $field, string $type, string $pattern, string $option): void
     {
         $this->addRawOption([
             'result' => $field,
